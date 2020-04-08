c66213f1695ce60a32e29657547370993500e54a
Emiliano-DLC <emiliano_r_d_l_c@outlook.com>
2020-04-08 14:54:10 -0600

    4/8/2020 recusrive quickSort and merge needed

--- java/src/newSorting.java
+++ java/src/newSorting.java
@@ -3,29 +3,50 @@ public class newSorting {
     public void newSorting(int[]a, int size){//size always the same
         if(a.length <= size){
             //quick sort
+            quickSort(a);
+
         }
         else {
-            int mid = a.length;
+            int mid = a.length/2;
             int[] leftHalf = new int[mid];
+            for(int i = 0; i<leftHalf.length; i++){
+                leftHalf[i]=a[i];
+            }
             int[] rightHalf = new int [a.length-mid];
+            for(int k = 0; k<rightHalf.length; k++){
+                rightHalf[k]=a[mid+k];
+            }
+            newSorting(leftHalf, size);
+            newSorting(rightHalf, size);
+            mergeSortedHalves(a,leftHalf,rightHalf);
 
         }
     }
 
+
     public void mergeSortedHalves(int[]a, int []left, int[] right){
         int leftCount = 0;
         int rightCount = 0;
-        for(int i = 0; i<a.length; i++) {
+        for(int i = 0; i<a.length+1; i++) {
             if (left[leftCount] < right[rightCount]) {
                 a[i] = left[leftCount];
                 leftCount++;
             }
-            if(right[rightCount]<left[leftCount]){
+            if(right[rightCount] < left[leftCount]){
                 a[i] = right[rightCount];
                 rightCount++;
             }
         }
     }
+
+    public void quickSort(int[]a){
+        quickSort(a, 0, a.length);
+    }
+
+    public void quickSort(int[] a, int start, int end){
+
+    }
+
 
 
 }
--- java/src/newSortingTest.java
+++ java/src/newSortingTest.java
@@ -1,5 +1,11 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class newSortingTest {
+    @Test
+    public void newSorting1(){
+
+    }
 
 }
