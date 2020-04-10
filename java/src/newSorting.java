ce35dbe1812977e2d4fea4da5022e1d1d84a9332
Emiliano-DLC <emiliano_r_d_l_c@outlook.com>
2020-04-10 17:47:34 -0600

    4/10/2020

--- java/src/newSorting.java
+++ java/src/newSorting.java
@@ -2,7 +2,6 @@ public class newSorting {
 
     public void newSorting(int[]a, int size){//size always the same
         if(a.length <= size){
-            //quick sort
             quickSort(a);
 
         }
@@ -24,35 +23,49 @@ public class newSorting {
     }
 
 
-    public void mergeSortedHalves(int[]a, int []left, int[] right){
-        int leftCount = 0;
-        int rightCount = 0;
-        for(int i = 0; i<a.length; i++) {
-            if(leftCount >= left.length){
-                a[i]=right[rightCount];
-                rightCount++;
-            }
-            if(rightCount >= right.length){
-                a[i]=left[leftCount];
-                leftCount++;
-            }
-            if(left[leftCount] < right[rightCount]){
-                a[i]=left[leftCount];
-                leftCount++;
-            }
-            if(right[rightCount] < left[leftCount]){
-                a[i]=right[rightCount];
-                rightCount++;
-            }
+    public void mergeSortedHalves(int[]a, int []left, int[]right){
+        mergeSorted(a,left,right,0,0,0);
+    }
+    public void mergeSorted(int[]a, int[]b, int[]c, int bCount, int cCount, int count) {
+        if (count == a.length - 1) {
+            return;
+        }
+        if (bCount >= b.length - 1) {
+            a[count] = c[cCount];
+            mergeSorted(a, b, c, bCount, cCount + 1, count + 1);
+        }
+        if (cCount >= c.length - 1 && bCount < b.length - 1) {
+            a[count] = b[bCount];
+            mergeSorted(a, b, c, bCount + 1, cCount, count + 1);
+        }
+        if (b[bCount] < c[cCount] && bCount < b.length - 1) {
+            a[count] = b[bCount];
+            mergeSorted(a, b, c, bCount + 1, cCount, count + 1);
+        }
+        if (c[cCount] < b[bCount] && cCount < c.length - 1) {
+            a[count] = c[cCount];
+            mergeSorted(a, b, c, bCount, cCount + 1, count + 1);
         }
     }
 
     public void quickSort(int[]a){
-        quickSort(a, 0, a.length);
+        quickSort(a, 1, a.length-1);
     }
 
     public void quickSort(int[] a, int start, int end){
-
+        int pivot = a[0];
+        while (a[start]<=pivot){
+            start++;
+        }
+        while (a[end]>=pivot && end > 0){
+            end--;
+        }
+        if(a[start] > pivot && a[end] < pivot) {
+            int temp = a[start];
+            a[start] = a[end];
+            a[end] = temp;
+            quickSort(a,1,a.length-1);
+        }
     }
 
 
--- java/src/newSortingTest.java
+++ java/src/newSortingTest.java
@@ -1,11 +1,54 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 class newSortingTest {
     @Test
-    public void newSorting1(){
+    //Test quick sort method with a unsorted array of integers
+    void newSorting1(){
+        newSorting test = new newSorting();
+        int[] a = {1,6,8,9,7,3};
+        int size = a.length;
+        test.newSorting(a,size);
+        assertArrayEquals(new int[]{1,3,6,7,8,9}, a);
+    }
+    @Test
+    //Test quick sort with a unsorted char array.
+    public void newSorting2(){
+        newSorting test = new newSorting();
+        int[] a = {'e','m','i','l','i','a','n','o'};
+        int size = a.length-1;
+        test.newSorting(a,size);
+        assertArrayEquals(new int[]{'a','e','i','i','m','n','o'}, a);
 
     }
+    @Test
+    //Test an unsorted array of char using merge sort.
+    public void newSorting3(){
+        newSorting test = new newSorting();
+        int[] a = {'e','m','i','l','i','a','n','o'};
+        int size = a.length+1;
+        test.newSorting(a,size);
+        assertArrayEquals(new int[]{'a','e','i','i','m','n','o'}, a);
+
+    }
+    @Test
+    //Test a mixture of negative and positive integers and also characters using quick sort.
+    public void newSorting4(){
+        newSorting test = new newSorting();
+        int[] a = {-1,'e', 4,-6,7,'d'};
+        int size = a.length-1;
+        test.newSorting(a,size);
+
+    }
+    @Test
+    //Test a mixture of negative and positive along with characters using merge sort.
+    public void newSorting5(){
+        newSorting test = new newSorting();
+        int[] a = {-1,'e', 4,-6,7,'d'};
+        int size = 1000;
+        test.newSorting(a,size);
+
+    }
+
 
 }
